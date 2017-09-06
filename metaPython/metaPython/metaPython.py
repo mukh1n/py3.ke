7cbb0f23e0f3100e3c70d7b7d3abc59b27318e26
Anton Mukhin <antonmu@123software.ru>
2017-09-06 12:13:06 +0300

    modifyAccountRenamed

--- metaPython/metaPython/metaPython.py
+++ metaPython/metaPython/metaPython.py
@@ -1,9 +1,13 @@
-from ModifyAccount import ModifyAccountAction
+from modifyAccount import *
 
 request1 = ModifyAccountAction.createDefault();
 request2 = ModifyAccountAction(1234, 'yobaGroup', 200)
 
-
+printer(request1)
 print('request1.toString() = ' + request1.toString())
 print('request2.toString() = ' + request2.toString())
 
+
+
+def printer(action):
+    print(action.toString())
