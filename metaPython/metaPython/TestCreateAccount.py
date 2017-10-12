cbe15eec7d3af42bf8287e34b8f35141fc2fc9d0
Anton Mukhin <antonmu@123software.ru>
2017-10-12 19:31:58 +0300

    cleanup

--- metaPython/metaPython/TestCreateAccount.py
+++ metaPython/metaPython/TestCreateAccount.py
@@ -1,18 +1,18 @@
-from ApiV1 import *
-from ApiProxy import *
+from metapi import *
+from apiProxy import *
 import unittest
 
 class TestCreateAccount(unittest.TestCase):
   def setUp(self):
-    apiProxy = FakeApiProxy()
-    self.api = Api(apiProxy)
+    proxy = FakeApiProxy()
+    self.client = ApiClient(proxy)
 
   def test_is_success(self):
-    result = self.api.createAccount('yoba', 'yobagroup', '1234qwer', comment = 'петух')
+    result = self.client.createAccount('yoba', 'yobagroup', '1234qwer', comment = 'петух')
     self.assertEqual(result.result, "1")
   
   def test_huypizda_not_empty(self):
-    result = self.api.createAccount('yoba', 'yobagroup', '1234qwer', comment = 'петух')
+    result = self.client.createAccount('yoba', 'yobagroup', '1234qwer', comment = 'петух')
     self.assertIsNotNone(result.huyPizda)
     
 if __name__ == '__main__':
