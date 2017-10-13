d19293b268d5e9fed36f3e09c223dc5fd402a412
Anton Mukhin <antonmu@123software.ru>
2017-10-13 12:11:31 +0300

    simplified

--- metaPython/metaPython/metapi/ApiClient.py
+++ metaPython/metaPython/metapi/ApiClient.py
@@ -1,14 +1,18 @@
 import metapi
 
 class ApiClient(object):
-    """description of class"""
-    def __init__(self, apiProxy):
-      self.apiProxy = apiProxy
+  """description of class"""
+  def __init__(self, apiProxy):
+    self.apiProxy = apiProxy
 
-    def createAccount(self, name, group, password
-          , login=None, agent=None, leverage=None, email=None, id=None, address=None, city=None, state=None
-          , zipcode=None, country=None, phone=None, password_phone=None, password_investor=None
-          , comment=None, enable=None, enable_change_password=None
-          , enable_read_only=None, send_reports=None, status=None):
-      result = metapi.Request(methon_name = 'create_account')(self.apiProxy, locals())
-      return metapi.Response(result)
+  def do(self, request):
+    result = self.apiProxy.get(request.string)
+    return metapi.Response(result)
+
+  def createAccount(self, name, group, password
+        , login=None, agent=None, leverage=None, email=None, id=None, address=None, city=None, state=None
+        , zipcode=None, country=None, phone=None, password_phone=None, password_investor=None
+        , comment=None, enable=None, enable_change_password=None
+        , enable_read_only=None, send_reports=None, status=None):
+    request = metapi.Request(methon_name = 'create_account', params = locals())
+    return self.do(request)
--- metaPython/metaPython/metapi/Response.py
+++ metaPython/metaPython/metapi/Response.py
@@ -1,19 +1,19 @@
 class Response(object):
-    """description of class"""
-    def __init__(self, stringResult):
-      self.__dict__ = self.parseResult(stringResult)
+  """description of class"""
+  def __init__(self, stringResult):
+    self.__dict__ = self.parseFormData(stringResult)
     
-    def __getattr__(self, name):
-      return self.__dict__.get(name)
+  def __getattr__(self, name):
+    return self.__dict__.get(name)
     
-    def parseResult(self, result):
-      dictionary = {}
-      keyValueStrings = result.split('&')
-      for keyValueString in keyValueStrings:
-        keyValue = keyValueString.split('=')
-        key = keyValue[0]
-        value = keyValue[1]
-        dictionary[key] = value
-      return dictionary
+  def parseFormData(self, result):
+    dictionary = {}
+    keyValueStrings = result.split('&')
+    for keyValueString in keyValueStrings:
+      keyValue = keyValueString.split('=')
+      key = keyValue[0]
+      value = keyValue[1]
+      dictionary[key] = value
+    return dictionary
 
 
